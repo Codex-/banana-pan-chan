fb619617a189cb05a27900def5d78f6934ba0a06
Alex Miller <codex.nz@gmail.com>
2020-06-05 23:31:25 +1200

    Add sql for adding and deleting commands

--- src/db/commands.ts
+++ src/db/commands.ts
@@ -13,3 +13,19 @@ export function getAllCommands(): CmdRow[] {
   const statement = db.prepare(sql);
   return statement.all();
 }
+
+export function addCommand(cmd: string, body: string): void {
+  const sql = `INSERT INTO ${TABLE} (${COLUMNS}) VALUES (@command, @body);`;
+  const statement = db.prepare(sql);
+  const tx = db.transaction(() => {
+    statement.run({ command: cmd, body: body });
+  });
+  tx();
+}
+
+export function deleteCommand(cmd: string): boolean {
+  const sql = `DELETE FROM ${TABLE} WHERE command = '${cmd}';`;
+  const statement = db.prepare(sql);
+  const result = statement.run();
+  return !!result.changes;
+}
